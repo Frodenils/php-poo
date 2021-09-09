c1eff3a7a79dfd0712237bc4a620171deb5fd6ef
FRODEDELAFORET Nils <frodnil@ndlp.fr>
2021-09-09 17:00:11 +0200

    added gets

--- Personnage.php
+++ Personnage.php
@@ -7,24 +7,82 @@ class Personnage
     private $_experience = 1;
     private $_degats = 0;
 
-    public function definirForce($force)
+    public function __construct($nom, $force = 50, $degats = 0)
     {
-        $this->_force = $force;
+        $this->setNom($nom);
+        $this->setForce($force);
+        $this->setDegats($degats);
+        $this->setExperience(1);
+        print("Le personnage " . $nom . " est créé </br>");
+    }
+    public function __toString()
+    {
+        return $this->getNom() . " (" . $this->getDegats() . ")";
     }
 
-    public function definirDegats($degats)
+    public function setNom($nom)
     {
+        if (!is_string($nom)) {
+            trigger_error('Le nom d\'un personnage doit être un texte', E_USER_ERROR);
+            return;
+        }
+        $this->_nom = $nom;
+    }
+    public function getNom()
+    {
+        return $this->_nom;
+    }
+    public function setForce($force)
+    {
+        if (!is_int($force)) {
+            trigger_error('La force d\'un personnage doit être un nombre entier', E_USER_WARNING);
+            return;
+        }
+        if ($force > 100) {
+            trigger_error('La force d\'un personnage ne peut pas dépasser 100', E_USER_WARNING);
+            return;
+        }
+        $this->_force = $force;
+    }
+    public function getForce()
+    {
+        return $this->_force;
+    }
+
+    public function setDegats($degats)
+    {
+        if (!is_int($degats)) {
+            trigger_error('Les degats d\'un personnage doivent être un nombre entier', E_USER_WARNING);
+            return;
+        }
         $this->_degats = $degats;
     }
 
-    public function afficherDegats()
+    public function getDegats()
     {
         return $this->_degats;
     }
 
-    public function definirExperience($experience)
+    public function setExperience($experience)
     {
+        if (!is_int($experience)) {
+            trigger_error('L\'expérience d\'un personnage doit être un nombre entier', E_USER_WARNING);
+            return;
+        }
+        if ($experience > 100) {
+            trigger_error('L\'expérience d\'un personnage ne peut pas dépasser 100', E_USER_WARNING);
+            return;
+        }
         $this->_experience = $experience;
+    }
+    public function gagnerExperience()
+    {
+        $this->_experience++;
+    }
+
+    public function getExperience()
+    {
+        return $this->_experience;
     }
 
     public function parler()
@@ -32,19 +90,12 @@ class Personnage
         print('Je suis un personnage!');
     }
 
-    public function frapper($adversaire)
+    public function frapper(Personnage $adversaire)
     {
         $adversaire->_degats += $this->_force;
         $this->gagnerExperience();
+        print('</br>' . $adversaire->getNom() . ' à été frappé par '
+            . $this . ' -> Dégats de ' . $adversaire . ' = ' . $adversaire->getDegats());
     }
 
-    public function gagnerExperience()
-    {
-        $this->_experience++;
-    }
-
-    public function afficherExperience()
-    {
-        return $this->_experience;
-    }
 }
--- index.php
+++ index.php
@@ -8,16 +8,16 @@ print("<h1>Jeu de Combat</h1>");
 //$perso1->gagnerExperience();
 //print("Expérience = " . $perso->afficherExperience());
 
-$perso1 = new Personnage();
-$perso1->definirForce(20);
-$perso1->definirExperience(15);
+$perso1 = new Personnage("Mario");
+//$perso1->definirForce(20);
+$perso1->setExperience(15);
 
-$perso2 = new Personnage();
-$perso2->definirForce(60);
-$perso2->definirExperience(1);
+$perso2 = new Personnage("Luigi", 60, 0);
+//$perso2->definirForce(60);
+$perso2->setExperience(1);
 
 $perso1->frapper($perso2);
 $perso2->frapper($perso1);
 
-print("<br/>Dégats du personnage 1 : " . $perso1->afficherDegats());
-print("<br/>Dégats du personnage 2 : " . $perso2->afficherDegats());
+print("<br/>Dégats du personnage 1 : " . $perso1->getDegats());
+print("<br/>Dégats du personnage 2 : " . $perso2->getDegats());
